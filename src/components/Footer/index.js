b20d55290e36b9adb5f3233f88d8ee8d14837579
Winterstorm <thunpwnz@gmail.com>
2018-12-02 21:48:17 +0400

    Basic structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React , { Component }from 'react';
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <h1>Here is some Footer</h1>
+        )
+    }
+}
+
+
+export default Footer;
---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React , { Component }from 'react';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <h1>Here is some Main Content</h1>
+        )
+    }
+}
+
+
+export default Main;
