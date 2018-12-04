40e61ac6971011cf0b0b589fdfc050f963b769c3
Winterstorm <thunpwnz@gmail.com>
2018-12-04 11:04:59 +0400

    found nice font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React , { Component }from 'react';
-
+import './header.css'
 
 class Header extends Component {
     render() {
         return (
-            <h1>Here is some Header</h1>
+            <header>
+                <h1 className='header'>Here is some Header</h1>
+                <h1 className='header'>Здесь будет заголовок</h1>
+            </header>
         )
     }
 }
