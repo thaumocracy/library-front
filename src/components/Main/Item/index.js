cd505adc03959c1e0e08b8f9fc28b708593dca7b
Winterstorm <thunpwnz@gmail.com>
2018-12-10 22:37:00 +0400

    conditional,example for it

--- src/components/Main/Item/index.js
+++ src/components/Main/Item/index.js
@@ -4,14 +4,15 @@ import './item.css'
 
 
 const Item = (props) => {
-
-
-    const { title , author , image , description } = props.package;
-
+    const { title , author , image , description , finished } = props.package;
+    let style = {}
+    if(finished){
+        style = {"color":"green"}
+    } 
     return (
         <div className="item">
-            <h3 className="item__title">{title}</h3>
-            <h3 className="item__title">{author}</h3>
+            <h3 className="item__title" style={style}>{title}</h3>
+            <h3 className="item__title" >{author}</h3>
             <div className="item__inner">
                 <img src={image} alt="Item" className='item__image item__left'/>
                 <p className="item__description item__right">
