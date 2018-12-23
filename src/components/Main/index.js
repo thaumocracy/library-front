bdc59bd654e34fbb37914109e9b6b23b4aba379e
Winterstorm <thunpwnz@gmail.com>
2018-12-23 22:10:22 +0400

    Adviser alpha

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,11 +6,16 @@ import './main.css'
 
 
 class Main extends Component {
+    adviseBook(){
+        const adviceArray = Books.filter(book => book.finished !== true);
+        const adviceItem = Math.floor(Math.random() * adviceArray.length);
+        const advice = adviceArray[adviceItem];
+    }
     render() {
         return (
             <main className="main__content">
                 <Category search={this.props.search} books={Books}/>
-                <Advisor />
+                <Advisor advice={this.adviseBook()}/>
             </main>
         )
     }
