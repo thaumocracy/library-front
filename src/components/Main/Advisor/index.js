e7130c75879e88233e5022747761a5d6732fe9d0
Winterstorm <thunpwnz@gmail.com>
2018-12-24 14:48:18 +0400

    Advisor working. Sort of

--- src/components/Main/Advisor/index.js
+++ src/components/Main/Advisor/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import "./advisor.css"
 
 
 const Advisor = (props) => {
-    return (
-        <h2>Advisor</h2>
-    )
+    const { advice , handleClose , show} = props;
+    if(show){
+        return (
+            <aside className="advisor__container">
+                <h2 className="advisor__item">{`Позвольте сегодня предложить вамъ чудесную книгу '${advice.title}' за авторствомъ ${advice.author} `}</h2>
+                <button className="advisor__close" onClick={() => handleClose()}>Сгинь къ дьяволу </button>
+            </aside>
+    
+        )
+    } else {
+        return null
+    }
+
 }
 
 
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,17 +6,36 @@ import './main.css'
 
 
 class Main extends Component {
-    adviseBook(){
-        const adviceArray = Books.filter(book => book.finished !== true);
+
+    state = {
+        advisor : true,
+        books : Books
+    }
+
+    adviseBook = () =>{
+        const adviceArray = this.state.books.filter(book => book.finished !== true);
         const adviceItem = Math.floor(Math.random() * adviceArray.length);
         const advice = adviceArray[adviceItem];
         return advice;
     }
+
+    handleClose = () => {
+        console.log(this.state)
+        const show = this.state.advisor
+        this.setState({advisor:!show})
+    }
+
     render() {
         return (
             <main className="main__content">
-                <Category search={this.props.search} books={Books}/>
-                <Advisor advice={this.adviseBook()}/>
+                <Category 
+                    search={this.props.search} 
+                    books={this.state.books}/>
+                <Advisor 
+                    show={this.state.advisor}
+                    advice={this.adviseBook()} 
+                    handleClose={this.handleClose}
+                />
             </main>
         )
     }
