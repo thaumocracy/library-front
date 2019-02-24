ac2e8e6c701143e5106f480cf46b26fdb943bbc7
Winterstorm <thunpwnz@gmail.com>
2019-02-24 23:16:21 +0400

    rethink a bit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app-container">
         <Header handleSearch={this.handleSearch}/>
         <Main search={this.state.search}/>
         <Footer />
--- src/components/Main/Item/index.js
+++ src/components/Main/Item/index.js
@@ -4,21 +4,17 @@ import './item.css'
 
 
 const Item = (props) => {
-    const { title , author , image , description , finished } = props.package;
-    const displayDescription = description.substr(0,240);
+    const { title , author , image , finished } = props.package;
     let style = {}
     if(finished){
         style = {"color":"green"}
     } 
     return (
         <div className="item">
-            <h3 className="item__title" style={style}>{title}</h3>
             <h3 className="item__title" >{author}</h3>
+            <h3 className="item__title" style={style}>{title}</h3>
             <div className="item__inner">
                 <img src={image} alt="Item" className='item__image item__left'/>
-                <p className="item__description item__right">
-                    {displayDescription}
-                </p>
             </div>
         </div>
     )
