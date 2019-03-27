ebd23a9df5018533956a5493018ef6181c585fe0
Winterstorm <thunpwnz@gmail.com>
2019-03-27 12:03:13 +0400

    webp images

--- src/components/Main/Item/index.js
+++ src/components/Main/Item/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from 'react'
 import './item.css'
 
-
-
 const Item = (props) => {
-    const { title , author , image , finished } = props.package;
-    let style = {}
-    if(finished){
-        style = {"color":"green"}
-    } 
-    return (
-        <div className="item">
-            <h3 className="item__title" >{author}</h3>
-            <h3 className="item__title" style={style}>{title}</h3>
-            <div className="item__inner">
-                <img src={image} alt="Item" className='item__image item__left'/>
-            </div>
-        </div>
-    )
+  const { title, author, image, finished } = props.package
+  let style = {}
+  if (finished) {
+    style = { 'color': 'green' }
+  }
+  return (
+    <div className='item'>
+      <h3 className='item__title' >{author}</h3>
+      <h3 className='item__title' style={style}>{title}</h3>
+      <div className='item__inner'>
+        <img src={image} alt='Item' className='item__image item__left' />
+      </div>
+    </div>
+  )
 }
 
-export default Item;
+export default Item
